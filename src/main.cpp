5353ea37a3895d3340dd99517c38d288fc9a2637
inspflof <155272834+inspflof@users.noreply.github.com>
2026-04-29 15:58:27 +0200

    plein de trucs

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,58 @@
 #include <Adafruit_MotorShield.h>
 #include <Stepper.h>
 #include <Servo.h>
+#include <AccelStepper.h>
+#include <Wire.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // ================== CONSTANTES ==================
 const int STEPS = 4096;   // 28BYJ-48
 
-const int PAS_PAR_CASE_X = STEPS;
+const int PAS_PAR_CASE_X = 500;
 const int PAS_PAR_CASE_Y = STEPS;
 const int PAS_Z = STEPS; // avance/recul prise voiture
 
 // ================== MOTEUR X ==================
-// Adafruit_StepperMotor *motorX = AFMS.getStepper(200, 3);
+Adafruit_MotorShield AFMS2 = Adafruit_MotorShield(0x61);
+Adafruit_StepperMotor *motorX = AFMS2.getStepper(200, 1);
+
+void stepX() {
+  motorX->onestep(FORWARD, DOUBLE);
+}
+
+void stepXBack() {
+  motorX->onestep(BACKWARD, DOUBLE);
+}
+
+AccelStepper stepperX(stepX, stepXBack);
 
 // ================== MOTEUR Y ==================
-Adafruit_StepperMotor *motorY = AFMS.getStepper(STEPS, 1);
-Adafruit_StepperMotor *motorY2 = AFMS.getStepper(STEPS, 2);
+Adafruit_MotorShield AFMS1 = Adafruit_MotorShield(0x60);
+Adafruit_StepperMotor *motorY = AFMS1.getStepper(STEPS, 1);
+Adafruit_StepperMotor *motorY2 = AFMS1.getStepper(STEPS, 2);
+
+void stepY1() {
+  motorY->onestep(FORWARD, DOUBLE);
+}
+
+void stepY1Back() {
+  motorY->onestep(BACKWARD, DOUBLE);
+}
+
+void stepY2() {
+  motorY2->onestep(BACKWARD, DOUBLE); // inversé
+}
+
+void stepY2Back() {
+  motorY2->onestep(FORWARD, DOUBLE); // inversé
+}
+
+AccelStepper stepperY1(stepY1, stepY1Back);
+AccelStepper stepperY2(stepY2, stepY2Back);
 
 // ================== Capteurs fin de course =============
 
-#define ENDSTOP_X 2
-#define ENDSTOP_Y 3
+#define ENDSTOP_X A2
+#define ENDSTOP_Y A3
 #define ENDSTOP_Z A4
 
 // ================== MOTEUR Z ==================
@@ -43,95 +75,136 @@ Servo servoZ2;
 int posX = 0;
 int posY = 0;
 
-
-
 // ================== DEPLACEMENTS ==================
 void moveX(int targetX) {
   int delta = targetX - posX;
-  int steps = delta * PAS_PAR_CASE_X;
-  Serial.println("deplacement X");
-  Serial.println(steps);
-  // if(delta > 0){
-  //   motorX->step(steps, FORWARD, DOUBLE);
-  // } else if (delta < 0) {
-  //   motorX->step(steps, BACKWARD, DOUBLE);
-  // }
+  if (delta == 0) return;
+
+  long steps = delta * PAS_PAR_CASE_X;
+
+  stepperX.move(-steps);
+
+  while (stepperX.distanceToGo() != 0) {
+    stepperX.run();
+  }
+
   posX = targetX;
+}
+
+void homeX() {
+  Serial.println("Homing X...");
+
+  // Sécurité timeout
+  unsigned long start = millis();
+
+  // On force une vitesse lente vers le ENDSTOP
+  stepperX.setMaxSpeed(50);
+  stepperX.setAcceleration(200);
+
+  // Mouvement continu vers le home (direction BACKWARD)
+  stepperX.moveTo(100000000);
+
+  while (digitalRead(ENDSTOP_X) == HIGH) {
+
+    // arrêt si timeout
+    if (millis() - start > 15000) {
+      Serial.println("ERREUR: fin de course X non detecte");
+      break;
+    }
+
+    stepperX.run();
+  }
+
+  Serial.println("Endstop X atteint");
+
+  // STOP
+  stepperX.setCurrentPosition(0);
+
+  delay(100);
+
+  // Petit recul pour relâcher le switch
+  stepperX.move(-20);
+
+  while (stepperX.distanceToGo() != 0) {
+    stepperX.run();
+  }
+
+  // Reset position logique
+  stepperX.setCurrentPosition(0);
+  posX = 0;
+
+  Serial.println("Homing X terminé");
 }
 
 void moveY(int targetY) {
   int delta = targetY - posY;
   if (delta == 0) return;
 
-  unsigned long totalSteps = abs(delta) * (unsigned long)PAS_PAR_CASE_Y;
-  
-  // Direction pour le moteur Y1
-  uint8_t dir1 = (delta > 0) ? FORWARD : BACKWARD;
-  // Direction opposée pour le moteur Y2
-  uint8_t dir2 = (delta > 0) ? BACKWARD : FORWARD; 
+  long steps = delta * PAS_PAR_CASE_Y;
 
-  Serial.println("Deplacement Y synchrone (miroir)...");
+  stepperY1.move(steps);
+  stepperY2.move(steps);
 
-  for (unsigned long i = 0; i < totalSteps; i++) {
-    motorY->onestep(dir1, DOUBLE);
-    motorY2->onestep(dir2, DOUBLE); // Le moteur 2 tourne à l'envers du moteur 1
-    
-    delayMicroseconds(800); // Ajuste ce délai si les moteurs forcent
+  while (stepperY1.distanceToGo() != 0) {
+    stepperY1.run();
+    stepperY2.run();
   }
-
-  delay(2000);
 
   posY = targetY;
 }
 
 void homeY() {
-  Serial.println("Homing Y...");
+  Serial.println("Homing Y (AccelStepper)...");
 
+  // Sécurité timeout
   unsigned long start = millis();
+
+  // On force une vitesse lente vers le ENDSTOP
+  stepperY1.setMaxSpeed(400);
+  stepperY1.setAcceleration(200);
+
+  stepperY2.setMaxSpeed(400);
+  stepperY2.setAcceleration(200);
+
+  // Mouvement continu vers le home (direction BACKWARD)
+  stepperY1.moveTo(-100000000);
+  stepperY2.moveTo(-100000000);
+
   while (digitalRead(ENDSTOP_Y) == HIGH) {
-    if (millis() - start > 10000) { // 10 sec timeout
+
+    // arrêt si timeout
+    if (millis() - start > 50000) {
       Serial.println("ERREUR: fin de course Y non detecte");
       break;
     }
-    motorY->onestep(BACKWARD, DOUBLE);
-    motorY2->onestep(FORWARD, DOUBLE); // miroir
-    delayMicroseconds(800);
+
+    stepperY1.run();
+    stepperY2.run();
   }
 
-  // petit recul
-  for (int i = 0; i < 50; i++) {
-    motorY->onestep(FORWARD, DOUBLE);
-    motorY2->onestep(BACKWARD, DOUBLE);
-    delayMicroseconds(800);
+  Serial.println("Endstop Y atteint");
+
+  // STOP
+  stepperY1.setCurrentPosition(0);
+  stepperY2.setCurrentPosition(0);
+
+  delay(100);
+
+  // Petit recul pour relâcher le switch
+  stepperY1.move(50);
+  stepperY2.move(50);
+
+  while (stepperY1.distanceToGo() != 0) {
+    stepperY1.run();
+    stepperY2.run();
   }
 
-  Serial.println("Fin de course Y atteint");
-
+  // Reset position logique
+  stepperY1.setCurrentPosition(0);
+  stepperY2.setCurrentPosition(0);
   posY = 0;
-}
 
-void homeX() {
-  Serial.println("Homing X...");
-
-  unsigned long start = millis();
-  while (digitalRead(ENDSTOP_X) == HIGH) {
-    if (millis() - start > 10000) { // 10 sec timeout
-      Serial.println("ERREUR: fin de course X non detecte");
-      break;
-    }
-    // motorX->onestep(BACKWARD, DOUBLE);
-    delayMicroseconds(800);
-  }
-
-  // petit recul
-  for (int i = 0; i < 50; i++) {
-    // motorX->onestep(FORWARD, DOUBLE);
-    delayMicroseconds(800);
-  }
-
-  Serial.println("Fin de course X atteint");
-
-  posX = 0;
+  Serial.println("Homing Y terminé");
 }
 
 void moveTo(int x, int y) {
@@ -139,13 +212,13 @@ void moveTo(int x, int y) {
   moveX(x);
 }
 
-
 void homeZ() {
   Serial.println("Homing Z...");
 
   unsigned long start = millis();
   // Aller vers le fin de course
   while (digitalRead(ENDSTOP_Z) == HIGH) {
+    Serial.println("Homing Z en cours...");
     motorZ.step(1);  // recule (à adapter si sens inversé)
 
     // sécurité timeout
@@ -168,10 +241,16 @@ void homeZ() {
 void takeCar() {
   Serial.println("Prise voiture");
   // motorZ.step(PAS_Z);
+  servoZ1.write(90);
+  delay(500);
+  motorZ.step(2700); 
+  delay(500);
   servoZ1.write(180);
-  
+  delay(500);
+  motorZ.step(-2700); 
+
+  Serial.println("Voiture prise");
   delay(2000);
-  // motorZ.step(-PAS_Z);
 }
 
 void dropCar() {
@@ -203,6 +282,12 @@ void fetchCar(int x, int y) {
 
 // ================== SETUP ==================
 void setup() {
+  Wire.begin();
+  Wire.setClock(400000); // 400 kHz au lieu de 100 kHz
+  Serial.begin(115200);
+
+  while (!Serial);
+  
   pinMode(ENDSTOP_X, INPUT_PULLUP);
   pinMode(ENDSTOP_Y, INPUT_PULLUP);
   pinMode(ENDSTOP_Z, INPUT_PULLUP);
@@ -211,47 +296,64 @@ void setup() {
   servoZ2.attach(SERVOZ2);
 
   servoZ1.write(90);
-
-  Serial.begin(115200);
-
-  while (!Serial);
-  Serial.println("Stepper test!");
-
-  // if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  // // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-  //   while (1) {
-  //     Serial.println("Could not find Motor Shield. Check wiring.");
-  //     delay(2000);
-  //   }
-  // }
-  // Serial.println("Motor Shield found.");
-
-  // // motorX->setSpeed(300);  // 10 rpm
-  // motorY->setSpeed(300);
-  // motorY2->setSpeed(300);
   
   motorZ.setSpeed(10);
+  
+  
+  if (!AFMS1.begin()) {
+    while (1) {
+      Serial.println("Shield 1 non detecte");
+      delay(1000);
+    }
+  }
 
-  // homeY();
-  // homeX();
+  if (!AFMS2.begin()) {
+    while (1) {
+      Serial.println("Shield 2 non detecte");
+      delay(1000);
+    }
+  }
+
+  stepperX.setMaxSpeed(1000);
+  stepperX.setAcceleration(300);
+  
+  stepperY1.setMaxSpeed(1000);
+  stepperY1.setAcceleration(300);
+
+  stepperY2.setMaxSpeed(1000);
+  stepperY2.setAcceleration(300);
+
+  // motorX->setSpeed(300);  // 10 rpm
+  motorY->setSpeed(300);
+  motorY2->setSpeed(300);
+  
+  Serial.println("JE VAIS A LA MAISON");
   // homeZ();
+  homeY();
+  homeX();
 
   Serial.println("Parking automatique prêt");
 }
 
 // ================== LOOP ==================
 void loop() {
-  Serial.println("test ok");
-  // Exemple : aller chercher la voiture en (2,1)
-  // fetchCar(2, 1);
-  // fetchCar(0, 0);
-
-  
-  Serial.println("test home Z");
-  takeCar();
-  
-
   delay(5000);
+
+  Serial.println("TEST X");
+
+  moveX(1);
+  delay(2000);
+
+  moveX(0);
+  delay(2000);
+
+  Serial.println("TEST Y");
+
+  moveY(1);
+  delay(2000);
+
+  moveY(0);
+  delay(2000);
 }
 
 
